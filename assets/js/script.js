6cf70246e1ef1752275460b7f93bc7514fbb2e03
Huy <drac0123451@gmail.com>
2022-07-28 21:24:53 -0700

    saving

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,22 +1,37 @@
+// declare countdown id for timer variable
 var timer = document.getElementById('countdown')
 
+// quiz timer
 function countdown() {
-    var timeLeft = 60;
+  // set initial time to 60 seconds
+  var timeLeft = 60;
   
-    // TODO: Add a comment describing the functionality of the setInterval() method: setintervall method to call function to be executed every 1000ms (1s)
-    var timeInterval = setInterval(function () {
-      // TODO: Add comments describing the functionality of the `if` statement: if timeleft is more than 1 timer will continue to count down with 'value>1 seconds remaining' plural
-      if (timeLeft > 1) {
-        timer.textContent = "Time: " + timeLeft;
-        timeLeft--;
-      } // TODO: Add comments describing the functionality of the `else` statement: clear the text content and time interval function then it will start the displayMessage function
-      else {
-        timer.textContent = 'Time: 0';
-        clearInterval(timeInterval);
-      }
-    }, 1000);
-  }
+  var timeInterval = setInterval(function () {
+    // if timeleft is more than 1 continue to countdown
+    if (timeLeft > 1) {
+      timer.textContent = "Time: " + timeLeft;
+      timeLeft--;
+    } 
+    // else display time: 0
+    else {
+      timer.textContent = 'Time: 0';
+      clearInterval(timeInterval);
+    }
+  }, 1000);
+};
 
+function startGame(quizIndex) {
+  
+}
 
+function setNextQuestion() {
 
+}
+
+function selectAnswer() {
+
+}
+
+startButton.addEventListener('click', function() {
   countdown();
+});
