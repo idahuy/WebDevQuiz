2f3de93929706cd04bdd37eca9ffcead416b561d
Huy <drac0123451@gmail.com>
2022-07-31 15:46:02 -0700

    touch ups

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,5 @@
-// declare countdown id for timer variable
-var timer = document.getElementById('countdown');
-
-// declared vars
-var score = 0;
-var quizArr = 0;
-var quiz = document.querySelector(".quiz");
-var container = document.querySelector(".container");
-var startButton = document.querySelector("#start");
-
-// declare array for objects of questions
+// Jamie Morris Homework-4 Code Quiz 
+// Var with array and object for questions 
 var questions = [
   {
       title: "Commonly used data types DO NOT include:",
@@ -37,150 +28,169 @@ var questions = [
   },
 
 ];
+// Declared variables
+var score = 0;
+var quizIndex = 0;
 
-// quiz timer
-function countdown() {
-  // set initial time to 60 seconds
-  var timeLeft = 60;
-  
-  var timeInterval = setInterval(function () {
-    // if timeleft is more than 1 continue to countdown
-    if (timeLeft > 1) {
-      timer.textContent = "Time: " + timeLeft;
-      timeLeft--;
-    } 
-    // else display time: 0
-    else {
-      timer.textContent = 'Time: 0';
-      clearInterval(timeInterval);
-    }
-  }, 1000);
-};
+// Start working code 
+// Declared variables
+var currentTime = document.querySelector("#currentTime");
+var timer = document.querySelector("#startTime");
+var quiz = document.querySelector("#quiz");
+var container = document.querySelector("#container");
 
-function startGame(quizArr) {
-  var makeUl = document.createElement("ul");
-  // wipes existing strings on page
+// declared variables for timer
+var timeLeft = 60;
+var timeInt = 0;
+var makeUl = document.createElement("ul");
+
+// Triggers timer on button, shows user a display on the screen
+timer.addEventListener("click", function () {
+  // If time is at 0 start the timer and start game
+  if (timeInt === 0) {
+      timeInt = setInterval(function () {
+          timeLeft--;
+          currentTime.textContent = "Time: " + timeLeft;
+
+          // if time drops to less than or equal to 0 end the quiz and display the message times up
+          if (timeLeft <= 0) {
+              clearInterval(timeInt);
+              endQuiz();
+              currentTime.textContent = "Time's up!";
+          }
+      }, 1000);
+  }
+  startGame(quizIndex);
+});
+
+// this function starts the game 
+function startGame(quizIndex) {
+  // clears the current text before running through the questions
   quiz.innerHTML = "";
   makeUl.innerHTML = "";
-  // loops through questions array
+
+  // the following for loop will loop through all questions in array
   for (var i = 0; i < questions.length; i++) {
-    var userQuestions = questions[quizArr].title;
-    var userChoices = questions[quizArr].choices;
-    quiz.textContent = userQuestions;
+      var userQuestion = questions[quizIndex].title;
+      var userChoices = questions[quizIndex].choices;
+      quiz.textContent = userQuestion;
   }
-
-  userChoices.forEach(function(item) {
-    var listEl = document.createElement("li");
-    listEl.textContent = item;
-    quiz.appendChild(makeUl);
-    makeUl.appendChild(listEl);
-    listEl.addEventListener("click", (check));
+  // New for each for question choices
+  userChoices.forEach(function (item) {
+      var listEl = document.createElement("li");
+      listEl.textContent = item;
+      quiz.appendChild(makeUl);
+      makeUl.appendChild(listEl);
+      listEl.addEventListener("click", (check));
   })
-};
+}
 
-// create a function to check user choices to correct answers
+// this function runs a check to see if user selected answer matches with answer found in the questions array
 function check(event) {
   var element = event.target;
 
   if (element.matches("li")) {
-    var divEl = document.createElement("div");
-    divEl.setAttribute("id", "divEl");
-    // check conditions
-    if (element.textContent == questions[quizArr].answer) {
-      // if user selected answer matches the correct answer in the array, add 10 to user's score and display the following message
-      score += 10;
-      divEl.textContent = "Correct"
-    } else {
-      // if user selected answer does not match the correct answer listed in the array, subtract 10 to the user's score and display the following message
-      timeLeft -= 10;
-      divEl.textContent = "Wrong"
-    }
-  }
-  // determines where you are currently in the array
-  quizArr++;
-  
-  if (quizArr >= questions.length) {
-    var totalScore = score + timeLeft;
-    // when the counter for quizarr reaches a number higher than the amount of question objects listed in array display the following string
-    endGame();
-    divEl.textContent = "End of the quiz, your score is " + totalScore;
-  } else {
-    startGame(quizArr);
-  }
-  quiz.appendChild(divEl);
-};
 
-
-// displays the end screen for the quiz
-function endQuiz() {
-  
-  var makeH3 = document.createElement("h3");
-  makeH3.setAttribute("class", "challenge");
-  makeH3.textContent = "All done!"
-  quiz.appendChild(makeH3);
-
-  var makeP = document.createElement("p");
-  quiz.appendChild(makeP);
-
-  // gets final score
-  if (totalScore >= 0) {
-    var scoreP = document.createElement("p");
-    scoreP.textContent = "Your final score is: " + totalScore;
-    quiz.appendChild(scoreP);
-  }
-
-  function enterInitials() {
-    // enter initials string
-    var initialsPrompt = document.createElement("label");
-    initialsPrompt.setAttribute("class", "initials");
-    initialsPrompt.textContent = "Enter your initials: ";
-    quiz.appendChild(initialsPrompt);
-
-    // user input for initials
-    var initialsInput = document.createElement("input");
-    initialsInput.setAttribute("type", "text");
-    initialsInput.setAttribute("class", "initialsInput");
-    initialsInput.setAttribute("placeholder", "Please enter initials...")
-    initialsInput.textContent = "";
-    quiz.appendChild(initialsInput);
-
-    // submit button for initials input
-    var submit = document.createElement("button");
-    submit.setAttribute("type", "submit");
-    submit.setAttribute("class", "submit");
-    submit.textContent = "Submit";
-    quiz.appendChild(submit);
-  }
-
-  // add an event listener for the button to capture initials and to add the initials + score to local storage
-  submit.addEventListener("click", function() {
-    var initials = initialsInput.value;
-
-    // will alert user if no value was entered continue to ask for initials until something is entered
-    if (initials === null || initials === "") {
-      window.alert("No value entered!");
-      enterInitials();
-    } else {
-      var highScore = {
-        initials: initials,
-        score: totalScore
-      }
-      var allScores = localStorage.getItem("allScores");
-      if (allScores = null) {
-        allScores = [];
+      var checkDiv = document.createElement("div");
+      checkDiv.setAttribute("id", "checkDiv");
+      // Correct condition 
+      if (element.textContent == questions[quizIndex].answer) {
+          score++;
+          checkDiv.textContent = "Correct!";
+      
+          
       } else {
-        allScores = JSON.parse(allScores);
+          // Will deduct -10 seconds of timeLeft for wrong answers
+          timeLeft -= 10;
+          checkDiv.textContent = "Wrong!";
       }
-      allScores.push(totalScore);
-      var newScore = JSON.stringify(allScores);
-      localStorage.setItem("allScores", newScore);
-      // takes you to highscore.html
-      window.location.replace("./highscores.html");
-    }
+
+  }
+  // Question Index determines number question user is on
+  quizIndex++;
+
+  if (quizIndex >= questions.length) {
+      // All done will append last page with user stats
+      endQuiz();
+      checkDiv.textContent = "End of quiz!" + " " + "You got  " + score + "/" + questions.length + " Correct!";
+  } else {
+      startGame(quizIndex);
+  }
+  quiz.appendChild(checkDiv);
+
+}
+// this function will end the game
+function endQuiz() {
+  quiz.innerHTML = "";
+  currentTime.innerHTML = "";
+
+  var endH1 = document.createElement("h1");
+  endH1.setAttribute("id", "endH1");
+  endH1.textContent = "All Done!" 
+  quiz.appendChild(endH1);
+
+
+  var endP = document.createElement("p");
+  endP.setAttribute("id", "endP");
+  quiz.appendChild(endP);
+
+  // gives you your final score if time>0
+  if (timeLeft >= 0) {
+      var timeRemaining = timeLeft;
+      var endP2 = document.createElement("p");
+      clearInterval(timeInt);
+      endP2.textContent = "Your final score is: " + timeRemaining;
+      quiz.appendChild(endP2);
+  }
+
+  // initials - Label
+  var initialsLabel = document.createElement("label");
+  initialsLabel.setAttribute("id", "initialsLabel");
+  initialsLabel.textContent = "Enter your initials: ";
+  quiz.appendChild(initialsLabel);
+
+  // initials - input
+  var initialsInput = document.createElement("input");
+  initialsInput.setAttribute("type", "text");
+  initialsInput.setAttribute("id", "initialsInput");
+  initialsInput.textContent = "";
+  quiz.appendChild(initialsInput);
+
+  // initials - submit
+  var submit = document.createElement("button");
+  submit.setAttribute("type", "submit");
+  submit.setAttribute("id", "submit");
+  submit.textContent = "submit";
+  quiz.appendChild(submit);
+
+  // eventlistener to capture initials and local storage for initials and score
+  submit.addEventListener("click", function () {
+      var initials = initialsInput.value;
+
+      if (initials === null) {
+
+          console.log("No value entered!");
+
+      } else {
+          var finalScore = {
+              initials: initials,
+              score: timeRemaining
+          }
+          console.log(finalScore);
+          var allScores = localStorage.getItem("allScores");
+          if (allScores === null) {
+              allScores = [];
+          } else {
+              allScores = JSON.parse(allScores);
+          }
+          allScores.push(finalScore);
+          var newScore = JSON.stringify(allScores);
+          localStorage.setItem("allScores", newScore);
+          // the following line will take you to highscores page
+          window.location.replace("highscores.html");
+      }
   });
+
 }
 
-startButton.addEventListener('click', function() {
-  countdown();
-  startGame(quizArr);
-});
+
